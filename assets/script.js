7d92bc8b8d8fc22cb6a160e3e21c57f368bf777c
Ryan Adams <ryanadams14@gmail.com>
2021-06-03 12:04:40 -0500

    finished append functionality and loops to adjust

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 var nonGTMBtn = document.getElementById("nonGTM-btn");
-
+var gtmBtn = document.getElementById("gtm-btn");
+var newFormBtn = document.getElementById("add-ref-form");
+var formGroup = document.getElementById("form-group");
+var gtmCopy = document.getElementById("gtm-copy")
+var nonGTMCopy
 
 function insertReferrerPolicy(tag) {
     var policy = "referrerpolicy='no-referrer-when-downgrade'"
@@ -20,10 +24,59 @@ function insertRefererParameter(tag, ref) {
 
 function updateNonGTMTag() {
     var nonGTMTag = document.getElementById("nogtm-tag").value;
-    alert(insertReferrerPolicy(nonGTMTag));
+    var output = insertReferrerPolicy(nonGTMTag)
+    var container = document.getElementById('non-gtm-output')
+    container.innerHTML = escapeTag(output);
+};
+
+function GTMTag() {
+    return document.getElementById("gtm-tag").value;
 }
 
+function referrers() {
+    var inputs = document.getElementsByClassName('parameter-input');
+    var params = [];
+    for (var i = 0; i < inputs.length; i++) {
+        params.push(inputs[i].value)
+    } return params;
+}
 
+function generateGTMTag(tag, ref) {
+    var output = insertRefererParameter(tag,encodeURIComponent(ref))
+    var container = document.getElementById('gtm-output')
+    container.innerHTML += escapeTag(output) + '<br>';
+}
 
+function escapeTag(str) {
+    return str.replace(/</,'&lt;').replace(/>/,'&gt;')
+}
+
+function generateGTMTags() {
+    var container = document.getElementById('gtm-output');
+    var tag = GTMTag();
+    var refs = referrers();
+    for (var i in refs) {
+        generateGTMTag(tag, refs[i])
+    }
+}
+
+function copyToClipboard() {
+    var copyText = document.getElementById('gtm-output')
+    copyText.select();
+    document.execCommand('copy');
+}
+
+function createNewForm() {
+    var newFormDiv = document.createElement("div");
+    var newForm = document.createElement("input");
+    newForm.className = "form-control mt-2  parameter-input";
+    newFormDiv.className = "input-group col-md-6";
+    newForm.setAttribute("placeholder", "Parameter Goes Here");
+    formGroup.appendChild(newFormDiv);
+    newFormDiv.appendChild(newForm);
+};
+
+// gtmCopy.addEventListener("click", copyToClipboard);
 nonGTMBtn.addEventListener("click", updateNonGTMTag);
-
+gtmBtn.addEventListener("click", generateGTMTags);
+newFormBtn.addEventListener("click", createNewForm);
